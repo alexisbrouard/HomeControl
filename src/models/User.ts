c4f68ba72153e05dc4551016f16a9b63d35a9ec4
alexisbrouard <alexis.brouard31@outlook.fr>
2022-04-08 15:13:56 +0200

    add zod for mails and regex username

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import z, { TypeOf } from "zod";
 
 const { Schema } = mongoose;
 
@@ -12,3 +13,9 @@ userSchema.set('toJSON', { virtuals: true });
 const user = mongoose.model("User", userSchema);
 
 export default user;
+
+export const userUpdate = z.object({
+  email: z.string().email(),
+  password: z.string().min(4),
+  username: z.string().min(2).regex(/^[a-zA-Z0-9]+$/),
+}); 
